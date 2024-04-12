a9d9b35c3bfa8109bc5e3eabdde5c836dcdc21d7
antoniomn01 <145062539+antoniomn01@users.noreply.github.com>
2024-04-12 12:23:43 +0200

    error

--- PracticaGit/PracticaGit/Program.cs
+++ PracticaGit/PracticaGit/Program.cs
@@ -5,9 +5,16 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            int a  = 1;
+            int a = 1;
             int b = 2;
-            Console.WriteLine(a+b);
+            Console.WriteLine(a + b);
+
+            for (int i = 0; i < a; i--)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
         }
+            
     }
 }
